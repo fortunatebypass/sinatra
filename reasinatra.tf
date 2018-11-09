f4e224e4b28cdd4dcd6859766c46d8c5258f6ef4
Sam Bailey <sam@thatsamguy.com>
2018-11-10 00:05:55 +1100

    initial reasinatra

---
+++ reasinatra.tf
@@ -0,0 +1,195 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+variable "ssh_from_public_ip_cidr" {
+  description = "Public IP address/CIDR to allow SSH access from E.g.: x.x.x.x/32"
+}
+
+variable "vpc_cidr" {
+  description = "IP CIDR for the VPC and single subnet - E.g.: 172.25.0.0/24"
+}
+
+variable "ssh_public_key" {
+  description = "SSH Public Key for initial EC2 SSH access - E.g.: 'ssh-rsa XXXXX user@host'"
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "ubuntu_bionic_minimal" {
+  most_recent = true
+
+  # Owner: Ubuntu
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "ubuntu-minimal/images/hvm-ssd/ubuntu-bionic-18.04-amd64-minimal-*",
+    ]
+  }
+}
+
+data "aws_ami" "intel_clear_linux" {
+  most_recent = true
+
+  # Owner: Intel
+  owners = ["679593333241"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "clear-*",
+    ]
+  }
+}
+
+resource "aws_key_pair" "reasinatra" {
+  key_name   = "reasinatra_key"
+  public_key = "${var.ssh_public_key}"
+}
+
+resource "aws_vpc" "vpc_name" {
+  cidr_block                       = "${var.vpc_cidr}"
+  assign_generated_ipv6_cidr_block = true
+
+  tags {
+    Name = "reasinatra_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "reasinatra_ig" {
+  vpc_id = "${aws_vpc.vpc_name.id}"
+
+  tags {
+    Name = "reasinatra_ig"
+  }
+}
+
+resource "aws_subnet" "vpc_sn" {
+  vpc_id                          = "${aws_vpc.vpc_name.id}"
+  cidr_block                      = "${var.vpc_cidr}"
+  ipv6_cidr_block                 = "${cidrsubnet(aws_vpc.vpc_name.ipv6_cidr_block, 8, 1)}"
+  assign_ipv6_address_on_creation = true
+  availability_zone               = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "vpc_sn"
+  }
+}
+
+resource "aws_route_table" "vpc_sn_rt" {
+  vpc_id = "${aws_vpc.vpc_name.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.reasinatra_ig.id}"
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = "${aws_internet_gateway.reasinatra_ig.id}"
+  }
+
+  tags {
+    Name = "vpc_sn_rt"
+  }
+}
+
+resource "aws_route_table_association" "vpc_sn_rt_assn" {
+  subnet_id      = "${aws_subnet.vpc_sn.id}"
+  route_table_id = "${aws_route_table.vpc_sn_rt.id}"
+}
+
+resource "aws_security_group" "public_sg" {
+  name        = "apse2_web_public_sg"
+  description = "Public Security group REA Web Instances"
+  vpc_id      = "${aws_vpc.vpc_name.id}"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    ipv6_cidr_blocks = [
+      "::/0",
+    ]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "${var.ssh_from_public_ip_cidr}",
+      "${aws_subnet.vpc_sn.cidr_block}",
+    ]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    ipv6_cidr_blocks = [
+      "${aws_subnet.vpc_sn.ipv6_cidr_block}",
+    ]
+  }
+
+  egress {
+    # allow all traffic out
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  egress {
+    # allow all traffic out
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+
+    ipv6_cidr_blocks = [
+      "::/0",
+    ]
+  }
+
+  tags {
+    Name = "apse2_web_public_sg"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "${data.aws_ami.ubuntu_bionic_minimal.id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${aws_subnet.vpc_sn.id}"
+  security_groups             = ["${aws_security_group.public_sg.id}"]
+  associate_public_ip_address = true
+  key_name                    = "${aws_key_pair.reasinatra.key_name}"
+  ipv6_address_count          = 1
+
+  tags {
+    "Name" = "reasinatra-web"
+  }
+}
+
+output "instance_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
