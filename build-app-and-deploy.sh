61e6dca03550a857490d491432b92f4de47e273e
Sam Bailey <sam@thatsamguy.com>
2018-11-10 23:34:43 +1100

    output friendly message

--- build-app-and-deploy.sh
+++ build-app-and-deploy.sh
@@ -7,3 +7,5 @@
 #
 export ANSIBLE_HOST_KEY_CHECKING=False;
 ansible-playbook -i "$(pwd)/ansible.inventory" --private-key="$HOME/.ssh/id_rsa" --extra-vars "host=all" "$(pwd)/build.yml";
+
+echo -e "\\nAPP URL: http://$(terraform output instance_ip)/";
